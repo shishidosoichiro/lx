1962558a749e8c8d8a66315abf542c8350c9e013
sshishido <sshishido>
2015-11-17 11:21:32 +0900

    'etc...'

--- lx.js
+++ lx.js
@@ -1,94 +1,180 @@
 ;(function(factory){
-	if (typeof module === 'object' && typeof module.exports === 'object') {
-		module.exports = factory();
-	}
+  if (typeof module === 'object' && typeof module.exports === 'object') {
+    module.exports = factory();
+  }
 })(function(){
-	var slice = function(array, begin, end){
-		return Array.prototype.slice.call(array, begin, end);
-	};
-	var array = function(value){
-		return value instanceof Array ? value : [value];
-	};
-	var functionalize = function(src){
-		return typeof src === 'function' ? src : function(){return src};
-	};
-	var shift = function(){
-		return slice(arguments, 1);
-	};
-	var flow = function(functions){
-		functions = functions.map(functionalize);
-		return function(){
-			var that = this
-			return functions.reduce(function(args, f){
-				return f.apply(that, array(args))
-			}, slice(arguments))
-		}
-	}
-	var noop = function(arg){return arg};
+  var noop = function(arg){return arg};
+  var slice = function(array, begin, end){
+    return Array.prototype.slice.call(array, begin, end);
+  };
+  var array = function(value){
+    return value instanceof Array ? value : [value];
+  };
+  var functionalize = function(src){
+    return typeof src === 'function' ? src : function(){return src};
+  };
+  var shift = function(){
+    return slice(arguments, 1);
+  };
+  var flow = function(functions){
+    functions = functions.map(functionalize);
+    return function(){
+      var that = this
+      return functions.reduce(function(args, f){
+        return f.apply(that, array(args))
+      }, slice(arguments))
+    }
+  }
+  var findIndex = function(array, callback, value){
+    var index;
+    var found;
+    if (typeof callback === 'function') {
+      found = array.some(function(el, i){
+        index = i;
+        return callback.apply(this, arguments)
+      })
+    }
+    if (typeof callback === 'undefined') {
+      found = array.some(function(el, i){
+        index = i;
+        return el
+      })
+    }
+    if (typeof callback === 'string') {
+      found = array.some(function(el, i){
+        index = i;
+        return el[callback] == value
+      })
+    }
+    return found ? index : undefined;
+  };
+  var captureCount = function(src){
+    return new RegExp('(?:' + source(src) + '|(any))').exec('any').length - 2;
+  };
+  var source = function(regex){
+    if (typeof regex === 'undefined') return '';
+    else if (typeof regex === 'string') return regex;
+    else if (typeof regex !== 'object') return regex;
 
-	var matcher = function(arg){
-		if (typeof arg === 'function') return arg;
-		if (arg instanceof RegExp) return function(string){return arg.exec(string)};
-		return function(){}
-	}
+    if (regex instanceof RegExp) return regex.source;
+    return regex.toString();
+  };
+  var call = function(src){
+    if (typeof src === 'function') return src.call(this);
+    return src;
+  };
+  var inherits = function(target, parent){
+    for (var i in parent) {
+      target[i] = parent[i];
+    }
+  };
 
-	var Lexer = function(){
-		var app = function(string){
-			var context = {}
-			context.state = app;
-			context.index = 0;
-			while (string != ''){
-				string = context.state.lex.call(context, string);
-			}
-		};
+  var defaults = {
+    flags: 'mg'
+  }
+  var count = 0;
 
-		var rules = [];
-		app.match = function(regex, action){
-			rules.push({matcher: matcher(regex), action: flow(slice(arguments, 1))})
-			return app;
-		};
-		var other = noop;
-		app.other = function(){
-			other = flow(slice(arguments, 1));
-			return app;
-		};
-		app.lex = function(string){
-			for (var i = 0; i < rules.length; i++) {
-				var matcher = rules[i].matcher;
-				var action = rules[i].action;
-				var matched = matcher(string);
-				if (!matched) continue;
+  var Lexer = function(options){
+    options = options || {};
+    inherits(options, defaults);
+    var app = function(string, context){
+      context = context || {};
+      context.state = app;
+      context.index = 0;
+      while (string != ''){
+        string = context.state.lex.call(context, string);
+      }
+    };
+    var id = count++;
 
-				// unmatchd part
-				if (matched.index != 0) {
-					other.call(this, string.substring(0, matched.index));
-				}
+    var store = (function(){
+      var sources = [];
+      var map = {};
 
-				// match part
-				this.index += matched.index;
-				action.apply(this, matched);
-				return string.slice(matched.index + matched[0].length);
-			}
+      return {
+        map: map,
+        sources: sources,
+        last: {position: 0, count: 1},
+        get: function(key){
+          return map[key];
+        },
+        put: function(key, rule){
+          map[key] = rule;
+          sources.push(rule.source)
+          this.last = rule;
+        },
+      }
+    })();
 
-			// unmatchd part
-			other.call(this, string);
-			return '';
-		}
-		return app;
+    var get = function(matched){
+      var position = findIndex(matched.slice(1)) + 1;
+      return store.get(position);
+    } 
+
+    var regex = function(){
+      var source = '(?:' + store.sources.map(function(src){
+        return '(' + call.call(this, src) + ')';
+      }, this).join('|') + ')';
+      return new RegExp(source, options.flags);
+    }
+    app.match = function(regex){
+      var position = store.last.position + store.last.count;
+      var src = source(regex);
+
+      var rule = {
+        source: src,
+        action: flow(slice(arguments, 1)),
+        count: captureCount(src) + 1,
+        position: position
+      }
+      store.put(position, rule);
+      return app;
+    };
+    var other = noop;
+    app.other = function(){
+      other = flow(slice(arguments));
+      return app;
+    };
+    app.lex = function(string){
+      var matched = regex.call(this).exec(string);
+      if (!matched) {
+        other.call(this, string, this.index);
+        return '';
+      }
+
+      var rule = get(matched);
+      var index = matched.index;
+
+      // unmatchd part
+      if (matched.index > 0) other.call(this, string.substring(0, matched.index));
+
+      // matched part
+      this.index += matched.index;
+      var captured = matched.slice(rule.position, rule.position + rule.count);
+      rule.action.apply(this, captured);
+
+      this.index += matched[0].length;
+      return string.substr(matched.index + matched[0].length);
+    }
+    return app;
+  };
+  Lexer.shift = shift;
+  Lexer.state = function(state){
+    return function(){
+      this.state = state;
+    }
+  } 
+  var toString = function(){
+    return this.name + '(' + this.index + (typeof this.value === 'undefined' ? '' : ':' + this.value) + ')';
+  }
+  Lexer.token = function(name){
+    return function(value){
+      return {name: name, value: value, index: this.index, toString: toString};
+    }
+  };
+	Lexer.push = function(token){
+		this.tokens.push(token)
 	};
-	Lexer.shift = shift;
-	Lexer.state = function(state){
-		return function(){
-			this.state = state;
-		}
-	} 
-	var toString = function(){
-		return this.name + '(' + value + ')';
-	}
-	Lexer.token = function(name){
-		return function(value){
-			return {name: name, value: value, toString: toString};
-		}
-	};
-	return Lexer;
+
+  return Lexer;
 })
--- test/lx.js
+++ test/lx.js
@@ -5,48 +5,71 @@ var Lexer = require('../lx');
 var state = Lexer.state;
 var shift = Lexer.shift;
 var token = Lexer.token;
-var log = function(){
-	console.log(arguments);
-	return arguments;
-};
+var push = Lexer.push;
 
 describe('lx.js', function(){
 
-	var xml;
+	var xml
 	before(function(){
-		xml = function(string){
-			var tokens = []
-			var push = tokens.push.bind(tokens);
-
-			var value = Lexer()
-			var attr = Lexer()
+		xml = (function(){
+			var content = Lexer()
 			var head = Lexer()
-			var body = Lexer()
+			var attr = Lexer()
+			attr.value = Lexer()
 
-			body
+			content
 			.match(/<([\w\.\:\-]+)/, shift, token('tag.head'), push, state(head))
 			.match(/<\/([\w\.\:\-]+)>/, shift, token('tag.tail'), push)
+			.other(token('text'), push)
 
 			head
 			.match(/\s+([\w\.\:\-]+)/, shift, token('attr.name'), push, state(attr))
-			.match(/>/, shift, token('tag.head.end'), push, state(body))
+			.match(/\/>/, token('tag.head.shortend'), push, state(content))
+			.match(/>/, token('tag.head.end'), push, state(content))
 
 			attr
-			.match(/=/, token('attr.name.end'), push, state(value))
-			.match(/>/, token('tag.head.end'), push, state(body))
+			.match(/=/, token('attr.name.end'), push, state(attr.value))
+			.match(/\/>/, token('tag.head.shortend'), push, state(content))
+			.match(/>/, token('tag.head.end'), push, state(content))
 
-			value
+			attr.value
 			.match(/\"([^\"]+)\"/, shift, token('attr.value'), push, state(head))
 			.match(/\'([^\']+)\'/, shift, token('attr.value'), push, state(head))
 			.match(/[\w\.\:\-]+/, token('attr.value'), push, state(head))
 
-			body(string);
-			return tokens;
-		};
+			return function(string){
+				var context = {tokens: []};
+				content(string, context);
+				return context.tokens;
+			}
+		})();
 	})
 
 	it('', function(){
-		var tokens = xml('<A><A a="b" c="d"></A><B></B><C e=\'f\'/></A>');
-		console.log(tokens);
+		var tokens = xml('<A><A a="b" c="d"></A><B></B>1 2=3 4<C e=\'f g\'  h=i/></A>');
+		expect(tokens[0].toString()).to.equal('tag.head(0:A)')
+		expect(tokens[1].toString()).to.equal('tag.head.end(2:>)')
+		expect(tokens[2].toString()).to.equal('tag.head(3:A)')
+		expect(tokens[3].toString()).to.equal('attr.name(5:a)')
+		expect(tokens[4].toString()).to.equal('attr.name.end(7:=)')
+		expect(tokens[5].toString()).to.equal('attr.value(8:b)')
+		expect(tokens[6].toString()).to.equal('attr.name(11:c)')
+		expect(tokens[7].toString()).to.equal('attr.name.end(13:=)')
+		expect(tokens[8].toString()).to.equal('attr.value(14:d)')
+		expect(tokens[9].toString()).to.equal('tag.head.end(17:>)')
+		expect(tokens[10].toString()).to.equal('tag.tail(18:A)')
+		expect(tokens[11].toString()).to.equal('tag.head(22:B)')
+		expect(tokens[12].toString()).to.equal('tag.head.end(24:>)')
+		expect(tokens[13].toString()).to.equal('tag.tail(25:B)')
+		expect(tokens[14].toString()).to.equal('text(29:1 2=3 4)')
+		expect(tokens[15].toString()).to.equal('tag.head(36:C)')
+		expect(tokens[16].toString()).to.equal('attr.name(38:e)')
+		expect(tokens[17].toString()).to.equal('attr.name.end(40:=)')
+		expect(tokens[18].toString()).to.equal('attr.value(41:f g)')
+		expect(tokens[19].toString()).to.equal('attr.name(46:h)')
+		expect(tokens[20].toString()).to.equal('attr.name.end(49:=)')
+		expect(tokens[21].toString()).to.equal('attr.value(50:i)')
+		expect(tokens[22].toString()).to.equal('tag.head.shortend(51:/>)')
+		expect(tokens[23].toString()).to.equal('tag.tail(53:A)')
 	})
 })

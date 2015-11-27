69ba65d2fc4a9031d0bf0136acc101db0ca6c98d
Soichiro Shishido <shishidosoichiro@hotmail.com>
2015-11-27 12:32:56 +0000

    test util

--- test/util.js
+++ test/util.js
@@ -49,6 +49,30 @@ describe('lib/util', function(){
 		expect(output[2]).to.equal('d');
 	})
 
+	describe('#flow', function(){
+		it('normal', function(){
+			var a = 'a';
+			var b = function(arg){return arg + 'b'};
+			var c = function(arg){return arg + 'c'};
+			var output = _.flow(a, b, c);
+			expect(output(1)).to.equal('abc');
+		})
+		it('array', function(){
+			var a = 'a';
+			var b = function(arg){return arg + 'b'};
+			var c = function(arg){return arg + 'c'};
+			var output = _.flow([a, b, c]);
+			expect(output(1)).to.equal('abc');
+		})
+		it('value', function(){
+			var a = function(arg){return arg + 'a'};
+			var b = function(arg){return arg + 'b'};
+			var c = function(arg){return arg + 'c'};
+			var output = _.flow(a, b, c);
+			expect(output(1)).to.equal('1abc');
+		})
+	})
+
 	describe('#findIndex', function(){
 		it('function style', function(){
 			var input1 = [
@@ -66,6 +90,11 @@ describe('lib/util', function(){
 			});
 			expect(output1).to.equal(1);
 		})
+		it('boolean style', function(){
+			var input1 = [false, true, true];
+			var output1 = _.findIndex(input1);
+			expect(output1).to.equal(1);
+		})
 		it('name-value style', function(){
 			var input1 = [
 				{
@@ -80,6 +109,74 @@ describe('lib/util', function(){
 			var output1 = _.findIndex(input1, 'name', 'b');
 			expect(output1).to.equal(1);
 		})
+		it('not found', function(){
+			var input1 = [
+				{
+					id: 1,
+					name: 'a'
+				},
+				{
+					id: 2,
+					name: 'b'
+				}
+			];
+			var output1 = _.findIndex(input1, 'name', 'c');
+			expect(output1).to.be.undefined;
+		})
 	})
 
+	describe('#source', function(){
+		it('undefined', function(){
+			var input = undefined;
+			var output = _.source(input);
+			expect(output).to.equal('');
+		});
+		it('string', function(){
+			var input = 'a';
+			var output = _.source(input)
+			expect(output).to.equal(input);
+		});
+		it('not object', function(){
+			var input = 1;
+			var output = _.source(input)
+			expect(output).to.equal(input);
+		});
+		it('regex', function(){
+			var input = /a/;
+			var output = _.source(input)
+			expect(output).to.equal(input.source);
+		});
+		it('object', function(){
+			var input = {toString: function(){return 'a'}};
+			var output = _.source(input)
+			expect(output).to.equal(input.toString());
+		});
+	})
+
+	it('#captureCount', function(){
+		var input = /a(b)((c)(d)e)f/;
+		var output = _.captureCount(input);
+		expect(output).to.equal(4);
+	})
+
+	describe('#call', function(){
+		it('not function', function(){
+			var input = 'a';
+			var output = _.call(input);
+			expect(output).to.equal(input);
+		});
+		it('function', function(){
+			var input = function(){return 'a'};
+			var output = _.call(input)
+			expect(output).to.equal(input());
+		});
+	})
+
+	it('#inherits', function(){
+		var parent = {a: 'b'};
+		var input = {c: 'd'};
+		var output = _.inherits(input, parent);
+		expect(input.a).to.equal(parent.a);
+		expect(input.c).to.equal(input.c);
+	})
 })
